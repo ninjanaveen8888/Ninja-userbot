746d3cfde508108291ecaf666d7201fdf644e7ae
ninjanaveen <67575446+ninjanaveen@users.noreply.github.com>
2020-06-29 12:48:54 +0530

    Update alive.py

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -8,13 +8,13 @@ from userbot.utils import admin_cmd
 
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Set ALIVE_NAME in config vars in Heroku"
-INDIANBOT_IS_ALIVE = ("**Ninja Naveen's Userbot** \nBOT Status : ` **`Pro Like Ninja Naveen**\n\n"
+INDIANBOT_IS_ALIVE = ("**Ninja Naveen's Userbot** \nBOT Status : ` `Pro Like Ninja Naveen**\n\n"
                      f"`My Pro owner`: {DEFAULTUSER}\n\n"
                      "`Bot Version:` [1.0](NinjaHackers.tk)\n`Python:` **3.7.4**\n"
                      "`Database Status:` **ALL OK**\n\n`Ninja Naveen's Pro Userbot\n`"
                      "**Bot Creator:** [🇮🇳INDIAN BHAI](t.me/pureindialover)\n"
                      "**Co-Owner:** [🇮🇳AKASH](t.me/AKASH_AM1)\n\n"
-                     "**Modified By:** [Ninja Naveen](t.me/ninjanaveen)\n\n"
+                     "**Modified By: [Ninja Naveen](t.me/ninjanaveen)\n\n"
                      "     [🇮🇳Deploy This Bot](https://github.com/ninjanaveen/IndianBot)") 
 
 
